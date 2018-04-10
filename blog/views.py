ec5711bd3e13e7db8a9768f482517eedea8c574a
cainanandrade <cainanandrade01@gmail.com>
2018-04-10 00:41:53 -0300

    Modificando o template para disponibilizar posts do banco de dados

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+#importando a classe Post
+from .models import Post
+
+#importando timezone
+from django.utils import timezone
+
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    
+    #querySet para publicar no site as postagens em ordem de criação:
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
